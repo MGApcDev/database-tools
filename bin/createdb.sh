a30d7a50c5960ed1ce1e30b028259eefd59ed139
man <man@dwarf.dk>
2021-04-30 13:00:21 +0200

    Add option for password on root user

--- bin/createdb.sh
+++ bin/createdb.sh
@@ -9,20 +9,19 @@ if [ -f "$FILE" ]; then
 
     echo "Creating database '${DB_DATABASE}' with user '${DB_USERNAME}'"
 
-
-    mysql -h${DB_HOST} -u${MYSQL_ROOT} -e "create database ${DB_DATABASE}" && \
+    MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" mysql -h${DB_HOST} -u${MYSQL_ROOT_USERNAME} -e "create database ${DB_DATABASE}" && \
     echo "Database created"
 
-    USER_EXISTS="$(mysql -u${MYSQL_ROOT} -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$DB_USERNAME')")"
+    USER_EXISTS="$(MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" mysql -u${MYSQL_ROOT_USERNAME} -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$DB_USERNAME')")"
     if [ "$USER_EXISTS" = 1 ]; then
-        echo "User already exist"
+       echo "User already exist"
     else
-        mysql -h${DB_HOST} -u${MYSQL_ROOT} -e "create user '${DB_USERNAME}'@'%' identified by '${DB_PASSWORD}'" && \
-        echo "User created"
+       MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" mysql -h${DB_HOST} -u${MYSQL_ROOT_USERNAME} -e "create user '${DB_USERNAME}'@'%' identified by '${DB_PASSWORD}'" && \
+       echo "User created"
     fi
 
-    mysql -h${DB_HOST} -u${MYSQL_ROOT} -e "grant all privileges on ${DB_DATABASE}.* to '${DB_USERNAME}'@'%'" && \
-    mysql -h${DB_HOST} -u${MYSQL_ROOT} -e "flush privileges"
+    MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" mysql -h${DB_HOST} -u${MYSQL_ROOT_USERNAME} -e "grant all privileges on ${DB_DATABASE}.* to '${DB_USERNAME}'@'%'" && \
+    MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" mysql -h${DB_HOST} -u${MYSQL_ROOT_USERNAME} -e "flush privileges"
     echo "Permissions to user granted"
 else
     echo "$FILE not found. Check you are in the correct directory."
